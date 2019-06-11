09d447b62db515c913d62aef47e5627a2de50093
zengyingjun <zengyingjun@ut.cn>
2019-06-11 11:52:08 +0800

    新增字符数组操作类

---
+++ 01-Src/SkeFramework.Core.IO/Enums/EndianEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkeFramework.Core.IO.Enums
+{
+    /// <summary>
+    /// 大小端枚举类
+    /// </summary>
+    [Flags]
+    public enum EndianEnum:int
+    {
+        BIG_ENDIAN=1,
+        LITTLE_ENDIAN=0
+    }
+}
