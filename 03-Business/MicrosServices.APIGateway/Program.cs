a944583806c5b38ae8e6e882cf2e960da35d8c71
0582zyj <502525164@qq.com>
2019-07-22 00:33:15 +0800

    新增心跳检查接口

--- 03-Business/MicrosServices.APIGateway/Program.cs
+++ 03-Business/MicrosServices.APIGateway/Program.cs
@@ -21,19 +21,19 @@ namespace MicrosServices.APIGateway
             var configuration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", false, true)
                 .Build();
-            var options = new ServiceDiscoveryOptions();
-            configuration.Bind("ServiceDiscovery", options);
-            var url = options.Service.GetUrl();
-            return Microsoft.AspNetCore.WebHost.CreateDefaultBuilder(args)
-                    .ConfigureAppConfiguration((hostingContext, builder) =>
-                    {
-                        builder.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
-                        .AddJsonFile("configuration.json", false, true);
-                    })
-                    .UseStartup<Startup>()
-                    .UseUrls(options.Service.GetUrl())
-                    .Build();
-
+            //var options = new ServiceDiscoveryOptions();
+            //configuration.Bind("ServiceDiscovery", options);
+            //var url = options.Service.GetUrl();
+            //return Microsoft.AspNetCore.WebHost.CreateDefaultBuilder(args)
+            //        .ConfigureAppConfiguration((hostingContext, builder) =>
+            //        {
+            //            builder.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+            //            .AddJsonFile("configuration.json", false, true);
+            //        })
+            //        .UseStartup<Startup>()
+            //        .UseUrls(options.Service.GetUrl())
+            //        .Build();
+            return null;
         }
     }
 }
