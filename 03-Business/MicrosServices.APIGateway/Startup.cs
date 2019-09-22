73be9eb620ebc6ae267cc8c47af88c6a17306e73
0582zyj <502525164@qq.com>
2019-09-22 14:31:20 +0800

    fix

--- 03-Business/MicrosServices.APIGateway/Startup.cs
+++ 03-Business/MicrosServices.APIGateway/Startup.cs
@@ -40,21 +40,16 @@ namespace MicrosServices.APIGateway
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        //// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        //public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        //{
-        //    if (env.IsDevelopment())
-        //    {
-        //        app.UseDeveloperExceptionPage();
-        //    }
-        //    else
-        //    {
-        //        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        //        app.UseHsts();
-        //    }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
-        //    app.UseHttpsRedirection();
-        //    app.UseMvc();
-        //}
+            app.UseMvc();
+            app.UseNacosAspNetCore();
+
+        }
     }
 }
