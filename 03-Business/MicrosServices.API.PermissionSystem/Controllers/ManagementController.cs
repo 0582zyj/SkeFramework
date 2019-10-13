5874aa7934f9403e0fb94645df4b14b6c9fd8acc
0582zyj <502525164@qq.com>
2019-10-13 23:53:25 +0800

    fix(权限中心)：修改分页页面类

--- 03-Business/MicrosServices.API.PermissionSystem/Controllers/ManagementController.cs
+++ 03-Business/MicrosServices.API.PermissionSystem/Controllers/ManagementController.cs
@@ -16,16 +16,26 @@ namespace MicrosServices.API.PermissionSystem.Controllers
     [ApiController]
     public class ManagementController : ControllerBase
     {
-        // GET api/values
+        #region 基础查询
+        /// <summary>
+        /// 获取全部权限列表
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public ActionResult<JsonResponses> GetManagementList()
         {
             List<PsManagement> managements = DataHandleManager.Instance().PsManagementHandle.GetList().ToList();
             return new JsonResponses(managements);
         }
-
+        /// <summary>
+        /// 分页获取权限列表
+        /// </summary>
+        /// <param name="pageIndex">当前第几页</param>
+        /// <param name="pageSize">每页多少行</param>
+        /// <param name="keywords">权限名称</param>
+        /// <returns></returns>
         [HttpGet]
-        public ActionResult<JsonResponses> GetManagementPageList(int pageIndex , int pageSize = 10, string keywords = "")
+        public ActionResult<JsonResponses> GetManagementPageList(int pageIndex , int pageSize = PageModel.DefaultPageSize, string keywords = "")
         {
             Expression<Func<PsManagement, bool>> where = null;
             if (!String.IsNullOrEmpty(keywords))
@@ -33,18 +43,59 @@ namespace MicrosServices.API.PermissionSystem.Controllers
                 where = (o => o.Name.Contains( keywords));
             }
             int total = Convert.ToInt32(DataHandleManager.Instance().PsManagementHandle.Count(where));//取记录数
-            if (pageIndex == 0)
-            {
-                pageSize = total> pageSize ? total: pageSize;
-            }
             PageModel page = new PageModel(pageIndex, pageSize, total);
             List<PsManagement> managements = DataHandleManager.Instance().PsManagementHandle
-                .GetDefaultPagedList(pageIndex, pageSize,where).ToList();
+                .GetDefaultPagedList(page.PageIndex, page.PageSize, where).ToList();
             var obj = new { pages = page, dataList = managements };
             return new JsonResponses(obj);
-
         }
+        /// <summary>
+        /// 根据主键ID获取信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<JsonResponses> GetManagementInfo(int id)
+        {
+            PsManagement Info = DataHandleManager.Instance().PsManagementHandle.GetModelByKey(id.ToString());
+            return new JsonResponses(Info);
+        }
+        #endregion
 
+        #region 增删改
+        /// <summary>
+        /// 新增提交方法
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult<JsonResponses> PsManagementAdd(PsManagement model)
+        {
+            var ResultCode = -1;
+            ResultCode = DataHandleManager.Instance().PsManagementHandle.ManagementInser(model);
+            return (ResultCode > 0 ?JsonResponses.Success:JsonResponses.Failed);
+        }
+        /// <summary>
+        /// 更新提交方法
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult<JsonResponses> ManagementUpdate(PsManagement model)
+        {
+            var ResultCode = -1;
+            ResultCode = DataHandleManager.Instance().PsManagementHandle.Update(model);
+            return (ResultCode > 0 ? JsonResponses.Success : JsonResponses.Failed);
+        }
+        /// <summary>
+        /// 删除提交方法
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult<JsonResponses> ManagementDelete(int id)
+        {
+            var ResultCode = -1;
+            ResultCode = DataHandleManager.Instance().PsManagementHandle.Delete(id);
+            return (ResultCode > 0 ? JsonResponses.Success : JsonResponses.Failed);
+        }
+        #endregion
 
     }
 }
--- 03-Business/MicrosServices.Entities/Responses/PageModel.cs
+++ 03-Business/MicrosServices.Entities/Responses/PageModel.cs
@@ -12,6 +12,10 @@ namespace MicrosServices.Entities.Responses
     public class PageModel
     {
         /// <summary>
+        /// 默认每页10行
+        /// </summary>
+        public const int DefaultPageSize = 10;
+        /// <summary>
         /// 当前页数
         /// </summary>
         public int PageIndex { get; set; }
@@ -32,6 +36,10 @@ namespace MicrosServices.Entities.Responses
         {
             this.PageIndex = pageIndex;
             this.PageSize = pageSize;
+            if (pageSize == 0)
+            {
+                pageSize = totalCount > pageSize ? totalCount : pageSize;
+            }
             this.TotalCount = totalCount;
             this.PageCount = (TotalCount + PageSize - 1) / PageSize;
         }
