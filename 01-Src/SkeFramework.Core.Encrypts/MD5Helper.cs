3aa676f357d3345e9e2dc67395790403659a5eac
0582zyj <502525164@qq.com>
2019-11-02 12:19:06 +0800

    feat(Ske框架)：新增框架常用的加密库

---
+++ 01-Src/SkeFramework.Core.Encrypts/MD5Helper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkeFramework.Core.Encrypts
+{
+    public class MD5Helper
+    {
+        /// <summary>
+        /// 字符串转换为MD5加密串
+        /// </summary>
+        /// <param name="originStr">未加密字符串</param>
+        /// <returns>已加密字符串</returns>
+        public static string GetMD5String(string originStr)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = System.Text.Encoding.Unicode.GetBytes(originStr);
+            byte[] targetData = md5.ComputeHash(fromData);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                sb.Append(targetData[i].ToString("x"));
+            }
+            return sb.ToString();
+        }
+    }
+}
