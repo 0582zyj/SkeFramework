bfe82b129fe814bbb993d5efa95e8fababe7124c
SunShall <502525164@qq.com>
2020-04-02 22:36:43 +0800

    fix(定时库)：新增定时库的扩展属性

---
+++ 01-Src/SkeFramework.Schedule.NetJob/DataAttributes/JobAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkeFramework.Schedule.NetJob.DataAttributes
+{
+      /// <summary>
+      /// 任务工具属性
+      /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class JobAttribute : Attribute
+    {
+    }
+}
