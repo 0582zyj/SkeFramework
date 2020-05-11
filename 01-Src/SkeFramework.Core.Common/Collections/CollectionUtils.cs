fa1efda423774fec7589e3421f03916ae69a6e7d
0582zyj <502525164@qq.com>
2020-05-11 17:12:07 +0800

    feat(基础库)：新增集合工具类

---
+++ 01-Src/SkeFramework.Core.Common/Collections/CollectionUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkeFramework.Core.Common.Collections
+{
+    /// <summary>
+    /// 集合工具类
+    /// </summary>
+    public static class CollectionUtils
+    {
+        /// <summary>
+        /// 判断是否为空
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static Boolean IsEmpty<T>(this IEnumerable<T> source)
+        {
+            if (source == null)
+                return true;
+            return !source.Any();
+        }
+    }
+}
