d3fcc982c03747aa21b6bf3543718caedb35c443
0582zyj <502525164@qq.com>
2020-05-15 14:51:19 +0800

    fix(框架工具库)：修改时间工具类

--- 01-Src/SkeFramework.Core.Common/DateTimes/DateTimeUtils.cs
+++ 01-Src/SkeFramework.Core.Common/DateTimes/DateTimeUtils.cs
@@ -1,5 +1,4 @@
-﻿using NodaTime;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,11 +16,53 @@ namespace SkeFramework.Core.Common.DateTimes
         /// 获取系统当前时间
         /// </summary>
         /// <returns>系统当前时间</returns>
-        public static DateTime GetNodaTimeNow()
+        public static DateTime GetSysDateTimeNow()
         {
-            Instant now = SystemClock.Instance.GetCurrentInstant();
-            var shanghaiZone = DateTimeZoneProviders.Tzdb["Asia/Shanghai"];
-            return now.InZone(shanghaiZone).ToDateTimeUnspecified();
+            return DateTime.Now;
+        }
+
+        /// <summary>
+        /// 获取系统当前时间格式化字符串 24小时制 被格式化为 (yyyy-MM-dd HH:mm:ss.fff)
+        /// </summary>
+        /// <returns>系统当前格式化的时间字符串(yyyy-MM-dd HH:mm:ss.fff)</returns>
+        public static string GetSysDateTimeNowStringYMD24HMSF()
+        {
+            return GetSysDateTimeNow().ToStringYMD24HMSF();
+        }
+
+        /// <summary>
+        /// 获取系统当前时间格式化字符串 12小时制 被格式化为 (yyyy-MM-dd hh:mm:ss.fff)
+        /// </summary>
+        /// <returns>系统当前格式化的时间字符串(yyyy-MM-dd hh:mm:ss.fff)</returns>
+        public static string GetSysDateTimeNowStringYMD12HMSF(this DateTime time)
+        {
+            return GetSysDateTimeNow().ToStringYMD12HMSF();
+        }
+
+        /// <summary>
+        /// 获取系统当前时间格式化字符串 24小时制 被格式化为 (yyyy-MM-dd HH:mm:ss)
+
+        /// </summary>
+        /// <returns>系统当前格式化的时间字符串(yyyy-MM-dd HH:mm:ss)</returns>
+        public static string GetSysDateTimeNowStringYMD24HMS(this DateTime time)
+        {
+            return GetSysDateTimeNow().ToStringYMD24HMS();
+        }
+        /// <summary>
+        /// 获取系统当前时间格式化字符串 12小时制 被格式化为 (yyyy-MM-dd hh:mm:ss)
+        /// </summary>
+        /// <returns>系统当前格式化的时间字符串(yyyy-MM-dd hh:mm:ss)</returns>
+        public static string GetSysDateTimeNowStringYMD12HMS(this DateTime time)
+        {
+            return GetSysDateTimeNow().ToStringYMD12HMS();
+        }
+        /// <summary>
+        /// 获取系统当前时间格式化字符串  被格式化为 (yyyy-MM-dd)
+        /// </summary>
+        /// <returns>系统当前格式化的时间字符串(yyyy-MM-dd)</returns>
+        public static string GetSysDateTimeNowStringYMD(this DateTime time)
+        {
+            return GetSysDateTimeNow().ToStringYMD();
         }
         #endregion
 
@@ -83,7 +124,7 @@ namespace SkeFramework.Core.Common.DateTimes
         public static long GetSecondTimestamp()
         {
             // 以1970-1-1 为时间开始 同系统当前时间的秒差值即为秒时间戳
-            TimeSpan ts = GetNodaTimeNow() - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            TimeSpan ts = GetSysDateTimeNow() - new DateTime(1970, 1, 1, 0, 0, 0, 0);
             return Convert.ToInt64(ts.TotalSeconds);
         }
         /// <summary>
@@ -93,7 +134,7 @@ namespace SkeFramework.Core.Common.DateTimes
         public static long GetMilliSecondTimestamp()
         {
             // 以1970-1-1 为时间开始 同系统当前时间的毫秒差值即为毫秒时间戳
-            TimeSpan ts = GetNodaTimeNow() - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            TimeSpan ts = GetSysDateTimeNow() - new DateTime(1970, 1, 1, 0, 0, 0, 0);
             return Convert.ToInt64(ts.TotalMilliseconds);
         }
         #endregion
@@ -104,8 +145,13 @@ namespace SkeFramework.Core.Common.DateTimes
         /// </summary>
         /// <param name="secondTimestamp">秒时间戳</param>
         /// <returns>转换后的时间</returns>
-        public static DateTime SecondStampToDateTime(long secondTimestamp)
+        public static DateTime? SecondStampToDateTime(long secondTimestamp)
         {
+            //  做一个简单的判断
+            if (secondTimestamp <= 0)
+            {
+                return null;
+            }
             // 以1970-1-1 为时间开始，通过计算与之的时间差，来计算其对应的时间
             DateTime dateTime = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
             dateTime = dateTime.AddSeconds(secondTimestamp).ToLocalTime();
@@ -117,8 +163,13 @@ namespace SkeFramework.Core.Common.DateTimes
         /// </summary>
         /// <param name="secondTimestampStr">字符串秒时间戳</param>
         /// <returns>转换后的时间</returns>
-        public static DateTime SecondStampToDateTime(string secondTimestampStr)
+        public static DateTime? SecondStampToDateTime(string secondTimestampStr)
         {
+            // 如果为空，那么直接返回null
+            if (string.IsNullOrEmpty(secondTimestampStr))
+            {
+                return null;
+            }
             // 首先将字符串时间戳转换为数字
             long secondTimestamp = 0;
             long.TryParse(secondTimestampStr, out secondTimestamp);
@@ -130,8 +181,13 @@ namespace SkeFramework.Core.Common.DateTimes
         /// </summary>
         /// <param name="secondTimestampStr">字符串毫秒时间戳</param>
         /// <returns>转换后的时间</returns>
-        public static DateTime MilliSecondStampToDateTime(long secondTimestamp)
+        public static DateTime? MilliSecondStampToDateTime(long secondTimestamp)
         {
+            //  做一个简单的判断
+            if (secondTimestamp <= 0)
+            {
+                return null;
+            }
             // 以1970-1-1 为时间开始，通过计算与之的时间差，来计算其对应的时间
             DateTime dateTime = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
             dateTime = dateTime.AddMilliseconds(secondTimestamp).ToLocalTime();
@@ -143,8 +199,13 @@ namespace SkeFramework.Core.Common.DateTimes
         /// </summary>
         /// <param name="milliSecondStampStr">毫秒时间戳</param>
         /// <returns>转换后的时间</returns>
-        public static DateTime MilliSecondStampToDateTime(string milliSecondStampStr)
+        public static DateTime? MilliSecondStampToDateTime(string milliSecondStampStr)
         {
+            // 如果为空，那么直接返回null
+            if (string.IsNullOrEmpty(milliSecondStampStr))
+            {
+                return null;
+            }
             // 首先将字符串时间戳转换为数字
             long milliSecondStamp = 0;
             long.TryParse(milliSecondStampStr, out milliSecondStamp);
