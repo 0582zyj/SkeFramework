cc6378539d4894896651cd3af4a65a92ad08caa3
0582zyj <502525164@qq.com>
2020-05-15 12:13:32 +0800

    fix(框架工具库)：新增枚举工具类

---
+++ 01-Src/SkeFramework.Core.Common/Enums/EnumUtils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkeFramework.Core.Common.Enums
+{
+    /// <summary>
+    /// 枚举工具类
+    /// </summary>
+    public static class EnumUtils
+    {
+        /// <summary>
+        /// 获取描述值
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static string GetEnumDescription(this Enum source)
+        {
+            Type typeDescription = typeof(DescriptionAttribute);
+            string strValue = source.ToString();
+            FieldInfo fieldinfo = source.GetType().GetField(strValue);
+            if (fieldinfo != null)
+            {
+                Object[] objs = fieldinfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+                if (objs != null)
+                {
+                    DescriptionAttribute da = (DescriptionAttribute)objs[0];
+                    return da.Description;
+                }
+            }
+            return strValue;
+        }
+
+    }
+}
