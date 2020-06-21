751753cd9ed08ba47945f2fd71eb4988e829e836
0582zyj <502525164@qq.com>
2020-06-21 21:53:26 +0800

    fix(登录枚举)：修改返回code参数枚举

--- 03-Business/MicrosServices.Helper.Core/Constants/LoginResultType.cs
+++ 03-Business/MicrosServices.Helper.Core/Constants/LoginResultType.cs
@@ -17,18 +17,20 @@ namespace MicrosServices.Helper.Core.Constants
         [EnumAttribute("账号停用")] ERROR_USER_INACTIVE = 912,
         [EnumAttribute("密码输入错误次数超过三次")] ERROR_PASSWORD_TOO_MUCH = 913,
         [EnumAttribute("密码错误")] ERROR_PASSWORD = 914,
-        [EnumAttribute("登录成功")] SUCCESS_LOGIN = 200,
+        [EnumAttribute("成功")] SUCCESS = 200,
+        [EnumAttribute("失败")] FAILED = 400,
+        [EnumAttribute("登录成功")] SUCCESS_LOGIN = 201,
 
 
-        [EnumAttribute("注册成功")] SUCCESS_REGISTOR = 200,
-        [EnumAttribute("注册失败")] FAILED_REGISTOR = 400,
+        [EnumAttribute("注册成功")] SUCCESS_REGISTOR = 202,
+        [EnumAttribute("注册失败")] FAILED_REGISTOR = 401,
 
         [EnumAttribute("邮箱不存在")] ERROR_EMAIL_NOT_EXIST = 810,
         [EnumAttribute("电话号码不存在")] ERROR_PHONE_NOT_EXIST = 811,
 
 
-        [EnumAttribute("注销成功")] SUCCESS_CANCEL = 200,
-        [EnumAttribute("注销失败")] FAILED_CANCEL = 400,
+        [EnumAttribute("注销成功")] SUCCESS_CANCEL = 203,
+        [EnumAttribute("注销失败")] FAILED_CANCEL = 402,
 
     }
 }
