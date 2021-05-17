35400fec31cb2cc306ec59fba3c76a4a27ba1c5e
0582zyj <502525164@qq.com>
2021-05-17 16:55:48 +0800

    feat(管理后台)：完成字典类型和字典的管理

---
+++ 03-Business/MicrosServices.SDK.AdminSystem/DictionaryTypeSDK.cs
@@ -0,0 +1,211 @@
+﻿using MicrosServices.Entities.Common.BaseSystem;
+using MicrosServices.Helper.Core.Extends;
+using Newtonsoft.Json;
+using SkeFramework.Core.Network.DataUtility;
+using SkeFramework.Core.Network.Enums;
+using SkeFramework.Core.Network.Https;
+using SkeFramework.Core.Network.Requests;
+using SkeFramework.Core.Network.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrosServices.SDK.AdminSystem
+{
+   public class DictionaryTypeSDK
+    {
+        private static readonly string GetListUrl = NetwordConstants.Instance().GetBaseUrl() + "/api/dictionarytype/getList";
+        private static readonly string GetPageUrl = NetwordConstants.Instance().GetBaseUrl() + "/api/dictionarytype/getPageList";
+        private static readonly string GetInfoUrl = NetwordConstants.Instance().GetBaseUrl() + "/api/dictionarytype/getInfo";
+        private static readonly string AddUrl = NetwordConstants.Instance().GetBaseUrl() + "/api/dictionarytype/create";
+        private static readonly string DeleteUrl = NetwordConstants.Instance().GetBaseUrl() + "/api/dictionarytype/delete";
+        private static readonly string UpdateUrl = NetwordConstants.Instance().GetBaseUrl() + "/api/dictionarytype/update";
+        private static readonly string GetOptionValueUrl = NetwordConstants.Instance().GetBaseUrl() + "/api/dictionarytype/getOptionValues";
+
+        /// <summary>
+        /// 获取菜单所有列表
+        /// </summary>
+        /// <param name="loginInfo"></param>
+        /// <returns></returns>
+        public List<BsDictionaryType> GetList()
+        {
+            List<BsDictionaryType> menus = new List<BsDictionaryType>();
+            try
+            {
+                RequestBase request = RequestBase.Get.Clone() as RequestBase;
+                request.Url = GetListUrl;
+                string result = HttpHelper.Example.GetWebData(request);
+                JsonResponses responses = JsonConvert.DeserializeObject<JsonResponses>(result);
+                if (responses.code == JsonResponses.SuccessCode)
+                {
+                    object data = responses.data;
+                    return JsonConvert.DeserializeObject<List<BsDictionaryType>>(JsonConvert.SerializeObject(data));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return menus;
+        }
+        /// <summary>
+        /// 获取菜单所有列表
+        /// </summary>
+        /// <param name="loginInfo"></param>
+        /// <returns></returns>
+        public PageResponse<BsDictionaryType> GetPageList(PageModel page, string keywords = "", long DictionaryNo = -1)
+        {
+            PageResponse<BsDictionaryType> menus = new PageResponse<BsDictionaryType>();
+            try
+            {
+                RequestBase request = RequestBase.Get.Clone() as RequestBase;
+                request.SetValue("pageindex", page.PageIndex);
+                request.SetValue("pagesize", page.PageSize);
+                request.SetValue("keywords", keywords);
+                request.SetValue("queryNo", DictionaryNo);
+                request.Url = GetPageUrl;
+                string result = HttpHelper.Example.GetWebData(request);
+                JsonResponses responses = JsonConvert.DeserializeObject<JsonResponses>(result);
+                if (responses.code == JsonResponses.SuccessCode)
+                {
+                    object data = responses.data;
+                    menus = JsonConvert.DeserializeObject<PageResponse<BsDictionaryType>>(JsonConvert.SerializeObject(data));
+                    return menus;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return menus;
+        }
+        /// <summary>
+        /// 根据主键ID获取信息
+        /// </summary>
+        /// <returns></returns>
+        public JsonResponses GetInfo(int id)
+        {
+            try
+            {
+                RequestBase request = RequestBase.Get.Clone() as RequestBase;
+                request.SetValue("id", id.ToString());
+                request.Url = GetInfoUrl;
+                string result = HttpHelper.Example.GetWebData(request);
+                JsonResponses responses = JsonConvert.DeserializeObject<JsonResponses>(result);
+                if (responses.code == JsonResponses.SuccessCode)
+                {
+                    object data = responses.data;
+                    responses.data = JsonConvert.DeserializeObject<BsDictionaryType>(JsonConvert.SerializeObject(data));
+                }
+                return responses;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return JsonResponses.Failed;
+        }
+        /// <summary>
+        /// 新增
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public JsonResponses DictionaryTypeAdd(BsDictionaryType model)
+        {
+            try
+            {
+                RequestBase request = RequestBase.PostForm.Clone() as RequestBase;
+                request.SetValue("dicType", model.DicType);
+                request.SetValue("descriptions", model.Descriptions);
+                request.SetValue("platformNo", model.PlatformNo);
+                request.SetValue("enabled", model.Enabled);
+                request.SetValue("inputUser", model.InputUser);
+                request.SetValue("inputTime", model.InputTime);
+                request.SetValue("updateUser", model.UpdateUser);
+                request.SetValue("updateTime", model.UpdateTime);
+                request.Url = AddUrl;
+                string result = HttpHelper.Example.GetWebData(request);
+                return JsonConvert.DeserializeObject<JsonResponses>(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return JsonResponses.Failed;
+        }
+        /// <summary>
+        /// 修改
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public JsonResponses DictionaryTypeUpdate(BsDictionaryType model)
+        {
+            try
+            {
+                RequestBase request = RequestBase.PostForm.Clone() as RequestBase;
+                request.SetValue("dicType", model.DicType);
+                request.SetValue("descriptions", model.Descriptions);
+                request.SetValue("platformno", model.PlatformNo);
+                request.SetValue("enabled", model.Enabled);
+                request.SetValue("id", model.id);
+                request.Url = UpdateUrl;
+                string result = HttpHelper.Example.GetWebData(request);
+                return JsonConvert.DeserializeObject<JsonResponses>(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return JsonResponses.Failed;
+        }
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="menu"></param>
+        /// <returns></returns>
+        public JsonResponses DictionaryDelete(int id)
+        {
+            try
+            {
+                RequestBase request = RequestBase.PostForm.Clone() as RequestBase;
+                request.SetValue("id", id);
+                request.Url = DeleteUrl;
+                string result = HttpHelper.Example.GetWebData(request);
+                return JsonConvert.DeserializeObject<JsonResponses>(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return JsonResponses.Failed;
+        }
+        /// <summary>
+        /// 获取键值对
+        /// </summary>
+        /// <returns></returns>
+        public List<DictionaryOptionValue> GetDictionaryOptionValues(long PlatformNo)
+        {
+            try
+            {
+                RequestBase request = RequestBase.Get.Clone() as RequestBase;
+                request.Url = GetOptionValueUrl;
+                request.SetValue("platformNo", PlatformNo);
+                string result = HttpHelper.Example.GetWebData(request);
+                JsonResponses responses = JsonConvert.DeserializeObject<JsonResponses>(result);
+                if (responses.code == JsonResponses.SuccessCode)
+                {
+                    object data = responses.data;
+                    return JsonConvert.DeserializeObject<List<DictionaryOptionValue>>(JsonConvert.SerializeObject(data));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return new List<DictionaryOptionValue>();
+        }
+
+    }
+}
---
+++ 03-Business/PermissionSystem.UI.WebSites/Controllers/DictionaryTypeController.cs
@@ -0,0 +1,124 @@
+﻿using MicrosServices.Entities.Common.BaseSystem;
+using MicrosServices.Helper.Core.Common;
+using MicrosServices.Helper.Core.Extends;
+using MicrosServices.SDK.AdminSystem;
+using PermissionSystem.UI.WebSites.Global;
+using PermissionSystem.UI.WebSites.Models;
+using SkeFramework.Core.Network.DataUtility;
+using SkeFramework.Core.Network.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PermissionSystem.UI.WebSites.Controllers
+{
+    public class DictionaryTypeController : Controller
+    {
+        private DictionaryTypeSDK dictionaryTypeSDK = new DictionaryTypeSDK();
+        #region 基础页面
+        /// <summary>
+        /// 列表页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult DictionaryTypeList()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 更新页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult DictionaryTypeUpdate(int id)
+        {
+            return View();
+        }
+        /// <summary>
+        /// 新增页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult DictionaryTypeAdd()
+        {
+            return View();
+        }
+        #endregion 
+
+        #region Basic GET POST
+        /// <summary>
+        /// 根据主键ID获取信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public JsonResult GetBsDictionaryTypeInfo(int id)
+        {
+            BsDictionaryType Info = new BsDictionaryType();
+            JsonResponses responses = dictionaryTypeSDK.GetInfo(id);
+            if (responses.code == JsonResponses.SuccessCode)
+            {
+                Info = responses.data as BsDictionaryType;
+            }
+            return Json(Info, JsonRequestBehavior.AllowGet);
+        }
+        /// <summary>
+        /// 获取列表信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public JsonResult GetBsDictionaryTypeList(int curPage = 1, string keywords = "", long DictionaryNo = -1)
+        {
+            PageModel page = new PageModel(curPage);
+            PageResponse<BsDictionaryType> pageResponse = dictionaryTypeSDK.GetPageList(page, keywords, DictionaryNo);
+            return Json(new PageResponseView<BsDictionaryType>(pageResponse), JsonRequestBehavior.AllowGet);
+        }
+        /// <summary>
+        /// 新增提交方法
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public JsonResult BsDictionaryTypeAdd(BsDictionaryType model)
+        {
+            model.InputUser = AppBusiness.loginModel.UserNo;
+            model.UpdateUser = model.InputUser;
+            JsonResponses responses = dictionaryTypeSDK.DictionaryTypeAdd(model);
+            return Json(responses, JsonRequestBehavior.AllowGet);
+        }
+
+
+        /// <summary>
+        /// 更新提交方法
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public JsonResult BsDictionaryTypeUpdate(BsDictionaryType model)
+        {
+            model.UpdateUser = AppBusiness.loginModel.UserNo;
+            JsonResponses responses = dictionaryTypeSDK.DictionaryTypeUpdate(model);
+            return Json(responses, JsonRequestBehavior.AllowGet);
+        }
+        /// <summary>
+        /// 删除提交方法
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public JsonResult BsDictionaryTypeDelete(int id)
+        {
+            JsonResponses responses = dictionaryTypeSDK.DictionaryDelete(id);
+            return Json(responses, JsonRequestBehavior.AllowGet);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 获取列表信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public JsonResult GetOptionValues()
+        {
+            long PlatformNo = LoginModel.Instance().PlatformNo;
+            List<DictionaryOptionValue> optionValues = dictionaryTypeSDK.GetDictionaryOptionValues(LoginModel.Instance().PlatformNo);
+            return Json(optionValues, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
