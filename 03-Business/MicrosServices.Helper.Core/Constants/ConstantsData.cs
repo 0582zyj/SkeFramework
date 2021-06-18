720a861f76ff4ab5ff4fc561db5046a76c8da8ae
0582zyj <502525164@qq.com>
2021-06-18 09:12:50 +0800

    feat(权限系统)：更新菜单父节点时同时刷新其子节点下的路径

---
+++ 03-Business/MicrosServices.Helper.Core/Constants/ConstantsData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrosServices.Helper.Core.Constants
+{
+    public class ConstantsData
+    {
+        public const long DEFAULT_ID = -1;
+    }
+}
